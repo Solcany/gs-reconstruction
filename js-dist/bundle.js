2f8ea1daf144842302c2d0210df1e0270045035a
Unknown <solcany.m@gmail.com>
2020-08-23 10:31:11 +0200

    fix sound

--- js-dist/bundle.js
+++ js-dist/bundle.js
@@ -250,7 +250,9 @@ const register_template_changer = function() {
       interactionEmitterId: {type: 'string'},
       interactionEvent: {type: 'string'},
       templatePathKind: {type: 'string'},
-      parseAudio: {type: 'boolean', default: false}
+      parseAudio: {type: 'boolean', default: false},
+      audioPlayersIDs: {type: 'array', default: []},
+      audioKinds: {type: 'array', default: []}
    },
 
     // emitts: 'template_set'
@@ -261,6 +263,12 @@ const register_template_changer = function() {
       this.templatePathKind = this.data.templatePathKind
 
       this.handle_interaction();
+
+      if(this.data.parseAudio) {
+        setTimeout(function() {
+          this.handle_audio();
+        }.bind(this), 500)
+      }
     },
 
     handle_interaction: function() {
@@ -282,42 +290,54 @@ const register_template_changer = function() {
         ,100)
     },
 
-    manage_audio: function() {
+    handle_audio: function() {
       // play any audio present in the templates.
       // audio element <a-sound> needs to be created manually in DOM
       // fires when a new template is loaded, listens to 'template_set' event
+      const audioIDs = this.data.audioPlayersIDs
+      const audioKinds = this.data.audioKinds
 
-      const sound_el = document.getElementById("audio-player");
-      if (!sound_el) throw new Error("a-sound element wasn't found in the DOM, add <a-sound id='audio-player' src='' positional='false'></a-sound> to DOM")
+      var audioPlayers = []
 
-      const sound_player = sound_el.components.sound
-      sound_player.stopSound();
+      const template_html = this.el.components.template.el
 
-      const play_audio = function() {
+     // get all a-data elements from the current aframe template
+      const data_html = template_html.getElementsByTagName("a-data");
+      const data = Array.from(data_html);
+
+      for(i = 0; i < audioIDs.length; i++) {
+        const id = cleanDOMId(audioIDs[i])
+        const audioPlayer_el = document.getElementById(id)
+        const audioPlayer = audioPlayer_el.components.sound;
+        audioPlayers.push(audioPlayer)
+      }
+
+      const playAudio = function(audioPlayer, audioKind) {
+        console.log("sound!");
         // get the html content of the currently loaded aframe template
-        const template_html = this.el.components.template.el
-
-        // get all a-data elements from the current aframe template
-        const data_html = template_html.getElementsByTagName("a-data");
-        const data = Array.from(data_html);
-
-        // get audio datai
-        const audio = data.filter(datum => datum.getAttribute('kind') === 'audio')// === 'audio')
+        const audio = data.filter(datum => datum.getAttribute('kind') === audioKind)// === 'audio')
         if(audio.length === 0) console.warn("this template has no audio attached")
-
         //play attached audio (if there's any in the template)
         if(audio.length > 0) {
+          console.log("there should be sound")
           // WIP: currently plays only the first audio source in the template
           const src = audio[0].getAttribute('data')
-          sound_el.setAttribute('sound','src', src);
-          sound_player.playSound();
-
+          audioPlayer.el.setAttribute('sound','src', src);
+          console.log(audioPlayer);
+          audioPlayer.playSound();
         } else {
           return;
         }
       }
 
-      //this.el.addEventListener('template_set', play_audio.bind(this), false);
+      for(u = 0; u < audioPlayers.length; u++) {
+        const audioPlayer = audioPlayers[u]
+        const audioKind = audioKinds[u]
+
+        this.el.addEventListener('template_set', function() {
+          audioPlayer.stopSound();
+          playAudio(audioPlayer, audioKind)}, false);
+      }
     }
 
   });
